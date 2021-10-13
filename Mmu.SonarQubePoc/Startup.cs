bfd32c69decfdab776e60eaf886255d7d92cb34a
Matthias Müller <matthias.mueller@trivadis.com>
2021-10-13 13:32:36 +0200

    WIP

--- Mmu.SonarQubePoc/Startup.cs
+++ Mmu.SonarQubePoc/Startup.cs
@@ -24,6 +24,12 @@ namespace Mmu.SonarQubePoc
 
         }
 
+        [Obsolete]  // Noncompliant
+        public static void Tra2()
+        {
+
+        }
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
